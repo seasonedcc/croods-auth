2b71956fb169ffe961ba90d82bf388714f480d48
giovanisleite <sousa_giovani@hotmail.com>
2021-05-14 18:07:55 -0300

    Test AuthProvider

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { ProviderElement } from 'croods/dist/types/typeDeclarations'
 
 import useCurrentUser from './useCurrentUser'
---
+++ src/__tests__/AuthProvider.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { CroodsProvider } from "croods";
+
+import AuthProvider, { useUserFromContext } from "../AuthProvider";
+
+jest.mock("../useCurrentUser", () =>
+  jest.fn(() => [
+    { validating: false, currentUser: { id: 1, firstName: "Foobar" } },
+    () => new Promise(() => ({}))
+  ])
+);
+
+it("calls useSignIn ", () => {
+  const Component = () => {
+    const [{ currentUser }] = useUserFromContext();
+    return <div>{currentUser.firstName}</div>;
+  };
+
+  const tree = renderer
+    .create(
+      <CroodsProvider baseUrl="https://foobar.com/api">
+        <AuthProvider>
+          <Component />
+        </AuthProvider>
+      </CroodsProvider>
+    )
+    .toJSON();
+
+  expect(tree).toMatchInlineSnapshot(`
+    <div>
+      Foobar
+    </div>
+  `);
+});
